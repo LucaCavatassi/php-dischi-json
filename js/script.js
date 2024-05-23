a6533a334916dc3514d574110cecd5979b4fb452
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-23 16:56:12 +0200

    feat: created a json where to push fav

--- js/script.js
+++ js/script.js
@@ -9,7 +9,16 @@ createApp({
     created() {
             axios.get("http://localhost:8888/boolean/php-dischi-json/server.php").then((resp) => {
                     this.resultsArray = resp.data.results;
+                    // console.log(resp.data.results);
             });
 
-        }
-    }).mount('#app')
+    },
+
+    methods: {
+        // addToFav(index) {
+        //     this.resultsArray[index].liked = !this.resultsArray[index].liked
+
+        //     console.log(this.resultsArray[index].liked , this.resultsArray[index]);
+        // }
+    }
+}).mount('#app')
--- server.php
+++ server.php
@@ -1,15 +1,22 @@
 <?php
-
+// ALL
 $all_albums_string = file_get_contents("all_albums.json"); // STRING
 
 $all_albums_array = json_decode($all_albums_string, true); //CONVERTITO IN ARRAY
-// var_dump($all_albums_array);
+
+
+// FAVOURITE
+$favourite_string = file_get_contents("favourite.json");
+// var_dump($favourite_string);
+
+$favourite_array = json_decode($favourite_string, true); //CONVERTITO IN ARRAY
+// var_dump($_POST["liked-btn"]);   
+
 
 // CREO RISPOSTA DI "API"
 $response = [
     "results" => $all_albums_array   
 ];
-// var_dump($response);
 
 $response_json = json_encode($response);
 
