5ab64a124d5a3010f09b39b576e356a15bd96b8a
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-23 14:28:20 +0200

    feat: respone with key results + results converted from array to json + browser type of content command + echo

--- server.php
+++ server.php
@@ -3,4 +3,16 @@
 $all_albums_string = file_get_contents("all_albums.json"); // STRING
 
 $all_albums_array = json_decode($all_albums_string, true); //CONVERTITO IN ARRAY
-var_dump($all_albums_array);
+// var_dump($all_albums_array);
+
+// CREO RISPOSTA DI "API"
+$response = [
+    "results" => $all_albums_array   
+];
+// var_dump($response);
+
+$response_json = json_encode($response);
+
+header("Content-Type: application/json");
+
+echo $response_json;
