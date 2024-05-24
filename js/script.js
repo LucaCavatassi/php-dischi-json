eac81ff157a89a7325740c3711551cb838619d5b
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-24 13:44:47 +0200

    feat: bonus 1 working

--- js/script.js
+++ js/script.js
@@ -13,4 +13,20 @@ createApp({
             });
 
     },
+    methods: {
+        addToFav(index) {
+            const data = {
+                action: "liked",
+                album_index: index
+            };
+            axios.post("http://localhost:8888/boolean/php-dischi-json/server.php", data, {
+                headers: {
+                    "Content-type" : "multipart/form-data",
+                },
+            }).then((resp) => {
+                this.resultsArray = resp.data.results
+                console.log(resp.data.results);
+            })
+        },
+    },
 }).mount('#app')
--- server.php
+++ server.php
@@ -5,6 +5,14 @@ $all_albums_string = file_get_contents("all_albums.json"); // STRING
 $all_albums_array = json_decode($all_albums_string, true); //CONVERTITO IN ARRAY
 
 
+if (isset($_POST["action"]) && $_POST["action"] === "liked") {
+    $album_index = $_POST["album_index"];
+    $all_albums_array[$album_index]["favourite"] = !$all_albums_array[$album_index]["favourite"];
+
+    file_put_contents("all_albums.json", json_encode($all_albums_array));
+};
+
+// var_dump($all_albums_array[0]["favourite"]);
 
 // CREO RISPOSTA DI "API"
 $response = [
